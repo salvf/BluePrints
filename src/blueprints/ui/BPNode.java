c50a53164275537dda99ae73ebe4df488410c67f
salvf <terro@LapHP-AMDA9>
2019-04-30 23:41:11 -0500

    Implementing NodeListeners and Type Node control

--- src/blueprints/ui/BPNode.java
+++ src/blueprints/ui/BPNode.java
@@ -5,30 +5,65 @@
  */
 package blueprints.ui;
 
-import javafx.beans.property.SimpleObjectProperty;
+import java.util.ArrayList;
+
 /**
  *
  * @author terro
  */
-public class BPNode implements Node{
-        private final SimpleObjectProperty value = new SimpleObjectProperty();
-        private String type = ""; 
-        public BPNode(String TYPE){
-            this.type=TYPE;
+public class BPNode<T> implements Node<T>{
+
+        private T value; 
+        private final Class<T> type;
+        private final boolean isInput; 
+        private final ArrayList<OutputNodeListener> outlisteners= new ArrayList<>();
+        private final ArrayList<InputNodeListener> inlisteners= new ArrayList<>();
+        
+        public BPNode(Class<T> type,boolean isInput){
+            this.isInput=isInput;
+            this.type=type;
         }
         
         @Override
-        public void setValue(Object value){
-            this.value.set(value);
+        public void setValue(T value){
+            if(equalsType(value)){
+                T oldvalue = this.value;
+                this.value = value;
+                if(!isInput)
+                    outlisteners.forEach((l)->{
+                        l.OnChange(this, oldvalue, this.value);
+                    });
+                else 
+                    inlisteners.forEach((l)->{
+                        l.OnRecive(this, this.value);
+                    });
+            }else throw new NodeTypeException("The value of this node is of different type than the value entered");
         }
         
         @Override
-        public SimpleObjectProperty getValue(){
+        public T getValue(){
             return this.value;
         } 
         
         public String getType(){
-            return this.type;
+            return this.type.getSimpleName();
+        }
+                
+        public boolean addInputNodeListener(NodeAdapter listener){
+            return inlisteners.add(listener);
+        }
+        
+        public boolean addOutputNodeListener(OutputNodeListener listener){
+            return outlisteners.add(listener);
+        }
+        
+        public boolean isInput(){
+            return this.isInput;
+        }
+        
+        private boolean equalsType(T value){
+            return value.getClass().getSimpleName().toLowerCase().contains(
+                    getType().toLowerCase());
         }
 
 
