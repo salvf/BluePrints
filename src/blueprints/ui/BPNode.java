059d3afd13544d1f08a1d5b9f0ca296e4887a5ce
salvf <terro@LapHP-AMDA9>
2019-05-06 22:03:39 -0500

    UI Node class

--- src/blueprints/ui/BPNode.java
+++ src/blueprints/ui/BPNode.java
@@ -1,23 +1,38 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2019 Salvador Vera Franco.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package blueprints.ui;
 
+import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
 /**
  *
- * @author terro
+ * @author Salvador Vera Franco
  */
 public class BPNode<T> implements Node<T>{
 
         private T value; 
+        private boolean connection = false;
         private final Class<T> type;
         private final boolean isInput; 
+        private Ellipse2D.Double gnode;
         private final ArrayList<OutputNodeListener> outlisteners= new ArrayList<>();
         private final ArrayList<InputNodeListener> inlisteners= new ArrayList<>();
+        private final ArrayList<ConnectionListener> connectionlisteners= new ArrayList<>();
         
         public BPNode(Class<T> type,boolean isInput){
             this.isInput=isInput;
@@ -55,6 +70,57 @@ public class BPNode<T> implements Node<T>{
         
         public boolean addOutputNodeListener(OutputNodeListener listener){
             return outlisteners.add(listener);
+        }
+        
+        public boolean addConnetionListener(ConnectionListener listener){
+            return connectionlisteners.add(listener);
+        }
+        
+        public boolean removeInputNodeListener(NodeAdapter listener){
+            return inlisteners.remove(listener);
+        }
+        
+        public boolean removeOutputNodeListener(OutputNodeListener listener){
+            return outlisteners.remove(listener);
+        }
+        
+        public boolean removeConnetionListener(ConnectionListener listener){
+            return connectionlisteners.remove(listener);
+        }
+        
+        public Ellipse2D.Double setGraphNode(Ellipse2D.Double gnode){
+            return this.gnode = gnode;
+        }
+        
+        public Ellipse2D.Double getGraphNode(){
+            return this.gnode;
+        }
+        
+        public void setConnection(boolean conn, BPNode node){
+            this.connection = conn;
+            if(isConnected())
+                if(isInput())
+                    connectionlisteners.forEach(cnsmr->{
+                        cnsmr.onConnect(node, this);
+                    });
+                else 
+                    connectionlisteners.forEach(cnsmr->{
+                        cnsmr.onConnect(this, node);
+                    });
+            else 
+                if(isInput())
+                    connectionlisteners.forEach(cnsmr->{
+                        cnsmr.onDisconnect(node, this);
+                    });
+                else 
+                    connectionlisteners.forEach(cnsmr->{
+                        cnsmr.onDisconnect(this, node);
+                    });
+                
+        }
+        
+        public boolean isConnected(){
+            return this.connection;
         }
         
         public boolean isInput(){
