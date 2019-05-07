5962c10958e62d1b326aebcf2239e06e7e1f96f9
salvf <terro@LapHP-AMDA9>
2019-05-06 22:02:46 -0500

    Class implementing all node listeners

--- src/blueprints/ui/NodeAdapter.java
+++ src/blueprints/ui/NodeAdapter.java
@@ -1,16 +1,33 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2019 Salvador Vera Franco.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package blueprints.ui;
 
 /**
  *
- * @author terro
+ * @author Salvador Vera Franco
  */
-public abstract class NodeAdapter implements InputNodeListener, OutputNodeListener, ConnectionListener{
+public abstract class NodeAdapter implements ConnectionListener, InputNodeListener, OutputNodeListener {
     
+    public void onConnect(BPNode outnode, BPNode inputnode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void onDisconnect(BPNode outnode, BPNode inputnode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     public void OnRecive(BPNode sendernode, Object newValue, Object oldValue) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
@@ -21,14 +38,6 @@ public abstract class NodeAdapter implements InputNodeListener, OutputNodeListen
 
 
     public void OnSend(BPNode sender, BPNode receiver, Object Value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void onConnect(BPNode outnode, BPNode inputnode) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void onDisconnect(BPNode outnode, BPNode inputnode) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
